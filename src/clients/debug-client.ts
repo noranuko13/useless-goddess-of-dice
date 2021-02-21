c503955b3c87c8c3e7b5c2208a8132112e7e9e46
noranuko13 <noranuko13@gmail.com>
2021-02-21 13:49:43 +0900

    :notes: [Client] Verify the input value.

--- src/clients/debug-client.ts
+++ src/clients/debug-client.ts
@@ -2,10 +2,11 @@ import { injectable } from 'tsyringe'
 import { DiceRoller } from '../rollers'
 import readline from 'readline'
 import { ClientInterface } from '../clients'
+import { Config } from '../config'
 
 @injectable()
 export class DebugClient implements ClientInterface {
-  constructor (private roller: DiceRoller) {}
+  constructor (private config: Config, private roller: DiceRoller) {}
 
   rl: readline.Interface = readline.createInterface({
     input: process.stdin,
@@ -15,9 +16,17 @@ export class DebugClient implements ClientInterface {
 
   waitInput (): void {
     this.rl.question('入力: ', (answer) => {
+      if (!this.isValid(answer)) {
+        return
+      }
+
       const content = this.roller.roll(answer)
       console.log('出力: ' + content)
       this.waitInput()
     })
   };
+
+  private isValid (content: string): boolean {
+    return content.startsWith(this.config.getPrefix())
+  }
 }
