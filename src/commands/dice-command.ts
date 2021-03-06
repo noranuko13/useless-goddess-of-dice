91b750220d026d2e51ab501c7b03b10baf04f915
noranuko13 <noranuko13@gmail.com>
2021-03-06 21:45:48 +0900

    :notes: [NSidedDice] Add input validation when creating a DiceCommand instance.

--- src/commands/dice-command.ts
+++ src/commands/dice-command.ts
@@ -1,10 +1,19 @@
-export class DiceCommand {
+import { BadCommandError } from '../@error'
+import { Command } from './command'
+
+export class DiceCommand extends Command {
   private readonly ndm: string;
   private readonly time: number;
   private readonly side: number;
 
   constructor (ndm: string) {
-    const numbers = ndm.match(/^(\d+)d(\d+)$/) || []
+    super()
+
+    const numbers = ndm.match(/^(\d+)d(\d+)$/)
+    if (!numbers) {
+      throw new BadCommandError()
+    }
+
     this.ndm = numbers[0]
     this.time = parseInt(numbers[1])
     this.side = parseInt(numbers[2])
--- test/commands/dice.command.ts
+++ test/commands/dice.command.ts
@@ -1,5 +1,6 @@
 import assert from 'assert'
 import 'reflect-metadata'
+import { BadCommandError } from '../../src/@error'
 import { DiceCommand } from '../../src/commands'
 
 describe('DiceCommand', function () {
@@ -13,5 +14,14 @@ describe('DiceCommand', function () {
     assert.strictEqual(d6.getNdm(), '2d6')
     assert.strictEqual(d6.getTime(), 2)
     assert.strictEqual(d6.getSide(), 6)
+  })
+
+  it('Instance creation fails.', function () {
+    const fn = (str: string) => {
+      return new DiceCommand(str)
+    }
+    assert.throws(() => { fn('') }, BadCommandError)
+    assert.throws(() => { fn('2d') }, BadCommandError)
+    assert.throws(() => { fn('d6') }, BadCommandError)
   })
 })
