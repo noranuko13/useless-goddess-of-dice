ca1ebccff20fcdb5d824917558d1d626a0fa1ee4
noranuko13 <noranuko13@gmail.com>
2021-03-14 18:00:02 +0900

    :notes: [tslog] Resolve build error with TS7053.
    TS7053: Element implicitly has an 'any' type because expression of type 'string | number' can't be used to index type 'unknown'.

--- src/@service/logger-service.ts
+++ src/@service/logger-service.ts
@@ -1,22 +1,19 @@
 import { Logger } from 'tslog'
-import { ISettingsParam } from 'tslog/src/index'
 import { injectable } from 'tsyringe'
 import { Config } from '../config'
 
 @injectable()
 export class LoggerService {
-  private readonly settings: ISettingsParam;
   private readonly logger: Logger;
 
   constructor (private config: Config) {
-    this.settings = {
+    this.logger = new Logger({
       name: 'DEFAULT',
       displayDateTime: false,
       displayFunctionName: false,
       minLevel: config.getLogLevel(),
       colorizePrettyLogs: config.isDebug()
-    }
-    this.logger = new Logger(this.settings)
+    })
   }
 
   getLogger (): Logger {
