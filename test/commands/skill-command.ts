fe025d653f55f307ef0b22345cbe4611eaaece77
noranuko13 <noranuko13@gmail.com>
2021-04-11 08:47:12 +0900

    :notes: [Template] Change Japanese to English.

--- test/commands/skill-command.ts
+++ test/commands/skill-command.ts
@@ -7,39 +7,39 @@ describe('SkillCommand', function () {
     // 目星(80)   [ 5, 16, 80, 96 ]
     // 聞き耳(40) [ 5,  8, 40, 96 ]
 
-    it('3 目星(80) 聞き耳(40)  # クリティカル', function () {
+    it('3 目星(80) 聞き耳(40)  # Critical', function () {
       const command = new SkillCommand(['3', '目星', '(', '80', ')', '聞き耳', '(', '40', ')'])
       command.cast()
       const expected = ':black_circle: 3 = 3\n' +
-        ' 　 目星 :black_circle: ( 80 ) = 80 　 :star2: クリティカル！\n' +
-        ' 　 聞き耳 :black_circle: ( 40 ) = 40 　 :star2: クリティカル！\n'
+        ' 　 目星 :black_circle: ( 80 ) = 80 　 :star2: Critical!\n' +
+        ' 　 聞き耳 :black_circle: ( 40 ) = 40 　 :star2: Critical!\n'
       assert.strictEqual(command.toString(), expected)
     })
 
-    it('10 目星(80) 聞き耳(40)  # スペシャル, 成功', function () {
+    it('10 目星(80) 聞き耳(40)  # Special, Success', function () {
       const command = new SkillCommand(['10', '目星', '(', '80', ')', '聞き耳', '(', '40', ')'])
       command.cast()
       const expected = ':black_circle: 10 = 10\n' +
-        ' 　 目星 :black_circle: ( 80 ) = 80 　 :dizzy: スペシャル！\n' +
-        ' 　 聞き耳 :black_circle: ( 40 ) = 40 　 :green_circle: 成功！\n'
+        ' 　 目星 :black_circle: ( 80 ) = 80 　 :dizzy: Special!\n' +
+        ' 　 聞き耳 :black_circle: ( 40 ) = 40 　 :green_circle: Success!\n'
       assert.strictEqual(command.toString(), expected)
     })
 
-    it('90 目星(80) 聞き耳(40)  # 失敗', function () {
+    it('90 目星(80) 聞き耳(40)  # Failure', function () {
       const command = new SkillCommand(['90', '目星', '(', '80', ')', '聞き耳', '(', '40', ')'])
       command.cast()
       const expected = ':black_circle: 90 = 90\n' +
-        ' 　 目星 :black_circle: ( 80 ) = 80 　 :red_circle: 失敗！\n' +
-        ' 　 聞き耳 :black_circle: ( 40 ) = 40 　 :red_circle: 失敗！\n'
+        ' 　 目星 :black_circle: ( 80 ) = 80 　 :red_circle: Failure!\n' +
+        ' 　 聞き耳 :black_circle: ( 40 ) = 40 　 :red_circle: Failure!\n'
       assert.strictEqual(command.toString(), expected)
     })
 
-    it('97 目星(80) 聞き耳(40)  # ファンブル', function () {
+    it('97 目星(80) 聞き耳(40)  # Fumble', function () {
       const command = new SkillCommand(['97', '目星', '(', '80', ')', '聞き耳', '(', '40', ')'])
       command.cast()
       const expected = ':black_circle: 97 = 97\n' +
-        ' 　 目星 :black_circle: ( 80 ) = 80 　 :octopus: ファンブル！\n' +
-        ' 　 聞き耳 :black_circle: ( 40 ) = 40 　 :octopus: ファンブル！\n'
+        ' 　 目星 :black_circle: ( 80 ) = 80 　 :octopus: Fumble!\n' +
+        ' 　 聞き耳 :black_circle: ( 40 ) = 40 　 :octopus: Fumble!\n'
       assert.strictEqual(command.toString(), expected)
     })
   })
