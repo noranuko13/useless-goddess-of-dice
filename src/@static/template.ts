c45ca300b43c3945af7db1cc26c2aa201390e4b8
noranuko13 <noranuko13@gmail.com>
2021-04-11 06:02:49 +0900

    :notes: [Template] Delete Template.common().

--- src/@static/template.ts
+++ src/@static/template.ts
@@ -1,11 +1,6 @@
-type CommonType = typeof import('../../template/default.json')
 type TemplateType = typeof import('../../template/default.json')
 
 export class Template {
-  static get common (): CommonType {
-    return require('../../template/default.json')
-  }
-
   static get messages (): TemplateType {
     return require('../../template/default.json')
   }
--- test/@static/template.ts
+++ test/@static/template.ts
@@ -3,13 +3,6 @@ import 'reflect-metadata'
 import { Template } from '../../src/@static'
 
 describe('Template', function () {
-  describe('#common()', function () {
-    it('OK', function () {
-      assert.strictEqual(Template.common.levels.critical, ':star2: クリティカル！')
-      assert.strictEqual(Template.common.levels.fumble, ':octopus: ファンブル！')
-    })
-  })
-
   describe('#messages()', function () {
     it('OK', function () {
       assert.strictEqual(Template.messages.error.badCommand, '駄女神わかんにゃーい(´・ω・)')
