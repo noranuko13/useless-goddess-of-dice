2c31f4ef4d620c9b6abe7e559c276029697df072
noranuko13 <noranuko13@gmail.com>
2021-04-11 10:13:05 +0900

    :notes: [Test] Change words.

--- test/commands/skill-command.ts
+++ test/commands/skill-command.ts
@@ -4,42 +4,42 @@ import { SkillCommand } from '../../src/commands'
 
 describe('SkillCommand', function () {
   describe('#cast(), #toString()', function () {
-    // 目星(80)   [ 5, 16, 80, 96 ]
-    // 聞き耳(40) [ 5,  8, 40, 96 ]
+    // SpotHidden(80)   [ 5, 16, 80, 96 ]
+    // Listen(40) [ 5,  8, 40, 96 ]
 
-    it('3 目星(80) 聞き耳(40)  # Critical', function () {
-      const command = new SkillCommand(['3', '目星', '(', '80', ')', '聞き耳', '(', '40', ')'])
+    it('3 SpotHidden(80) Listen(40)  # Critical', function () {
+      const command = new SkillCommand(['3', 'SpotHidden', '(', '80', ')', 'Listen', '(', '40', ')'])
       command.cast()
       const expected = ':black_circle: 3 = 3\n' +
-        ' 　 目星 :black_circle: ( 80 ) = 80 　 :star2: Critical!\n' +
-        ' 　 聞き耳 :black_circle: ( 40 ) = 40 　 :star2: Critical!\n'
+        ' 　 SpotHidden :black_circle: ( 80 ) = 80 　 :star2: Critical!\n' +
+        ' 　 Listen :black_circle: ( 40 ) = 40 　 :star2: Critical!\n'
       assert.strictEqual(command.toString(), expected)
     })
 
-    it('10 目星(80) 聞き耳(40)  # Special, Success', function () {
-      const command = new SkillCommand(['10', '目星', '(', '80', ')', '聞き耳', '(', '40', ')'])
+    it('10 SpotHidden(80) Listen(40)  # Special, Success', function () {
+      const command = new SkillCommand(['10', 'SpotHidden', '(', '80', ')', 'Listen', '(', '40', ')'])
       command.cast()
       const expected = ':black_circle: 10 = 10\n' +
-        ' 　 目星 :black_circle: ( 80 ) = 80 　 :dizzy: Special!\n' +
-        ' 　 聞き耳 :black_circle: ( 40 ) = 40 　 :green_circle: Success!\n'
+        ' 　 SpotHidden :black_circle: ( 80 ) = 80 　 :dizzy: Special!\n' +
+        ' 　 Listen :black_circle: ( 40 ) = 40 　 :green_circle: Success!\n'
       assert.strictEqual(command.toString(), expected)
     })
 
-    it('90 目星(80) 聞き耳(40)  # Failure', function () {
-      const command = new SkillCommand(['90', '目星', '(', '80', ')', '聞き耳', '(', '40', ')'])
+    it('90 SpotHidden(80) Listen(40)  # Failure', function () {
+      const command = new SkillCommand(['90', 'SpotHidden', '(', '80', ')', 'Listen', '(', '40', ')'])
       command.cast()
       const expected = ':black_circle: 90 = 90\n' +
-        ' 　 目星 :black_circle: ( 80 ) = 80 　 :red_circle: Failure!\n' +
-        ' 　 聞き耳 :black_circle: ( 40 ) = 40 　 :red_circle: Failure!\n'
+        ' 　 SpotHidden :black_circle: ( 80 ) = 80 　 :red_circle: Failure!\n' +
+        ' 　 Listen :black_circle: ( 40 ) = 40 　 :red_circle: Failure!\n'
       assert.strictEqual(command.toString(), expected)
     })
 
-    it('97 目星(80) 聞き耳(40)  # Fumble', function () {
-      const command = new SkillCommand(['97', '目星', '(', '80', ')', '聞き耳', '(', '40', ')'])
+    it('97 SpotHidden(80) Listen(40)  # Fumble', function () {
+      const command = new SkillCommand(['97', 'SpotHidden', '(', '80', ')', 'Listen', '(', '40', ')'])
       command.cast()
       const expected = ':black_circle: 97 = 97\n' +
-        ' 　 目星 :black_circle: ( 80 ) = 80 　 :octopus: Fumble!\n' +
-        ' 　 聞き耳 :black_circle: ( 40 ) = 40 　 :octopus: Fumble!\n'
+        ' 　 SpotHidden :black_circle: ( 80 ) = 80 　 :octopus: Fumble!\n' +
+        ' 　 Listen :black_circle: ( 40 ) = 40 　 :octopus: Fumble!\n'
       assert.strictEqual(command.toString(), expected)
     })
   })
--- test/commands/skill-dice-command.ts
+++ test/commands/skill-dice-command.ts
@@ -6,19 +6,19 @@ import { SkillDiceCommand } from '../../src/commands'
 describe('SkillDiceCommand', function () {
   describe('#cast(), #toString()', function () {
     it('BadCommandError', function () {
-      assert.throws(() => { return new SkillDiceCommand(['目星', '80', ')']) }, BadCommandError)
+      assert.throws(() => { return new SkillDiceCommand(['SpotHidden', '80', ')']) }, BadCommandError)
     })
 
-    it('目星(80)', function () {
-      const command = new SkillDiceCommand(['目星', '(', '80', ')'])
+    it('SpotHidden(80)', function () {
+      const command = new SkillDiceCommand(['SpotHidden', '(', '80', ')'])
       command.cast()
-      assert.strictEqual(command.toString(), '目星 :black_circle: ( 80 ) = 80')
+      assert.strictEqual(command.toString(), 'SpotHidden :black_circle: ( 80 ) = 80')
     })
 
-    it('目星(60 + 2d6)', function () {
-      const command = new SkillDiceCommand(['目星', '(', '60', '+', '2d6', ')'])
+    it('SpotHidden(60 + 2d6)', function () {
+      const command = new SkillDiceCommand(['SpotHidden', '(', '60', '+', '2d6', ')'])
       command.cast()
-      assert.strictEqual(/目星 :black_circle: \( 60 \+ 2d6<[1-6],[1-6]> \) = \d+/.test(command.toString()), true)
+      assert.strictEqual(/SpotHidden :black_circle: \( 60 \+ 2d6<[1-6],[1-6]> \) = \d+/.test(command.toString()), true)
     })
   })
 })
