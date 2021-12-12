adc1e0fa6a97a60fb9060151082f392b75df0557
noranuko13 <noranuko13@gmail.com>
2021-12-12 19:44:50 +0900

    :notes: [Node.js] useUnknownInCatchVariables

--- src/@static/formula.ts
+++ src/@static/formula.ts
@@ -6,8 +6,12 @@ export class Formula {
     try {
       const formula = content.replace(/\d+d\d+/g, '0')
       evaluate(formula)
-    } catch (error) {
-      throw new WrongFormulaError(error.message)
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new WrongFormulaError(error.message)
+      } else {
+        throw error
+      }
     }
   }
 }
